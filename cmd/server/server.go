fd7d82c64dd424729b7de8f3209c82fddf66ce89
leejoys <44557202+leejoys@users.noreply.github.com>
2021-10-13 00:35:55 +0300

    os.Interrupt

--- cmd/server/server.go
+++ cmd/server/server.go
@@ -11,6 +11,7 @@ import (
 	"newssrv/pkg/storage"
 	"newssrv/pkg/storage/pgdb"
 	"os"
+	"os/signal"
 	"time"
 )
 
@@ -63,9 +64,14 @@ func main() {
 
 	// Запускаем веб-сервер на порту 8081 на всех интерфейсах.
 	// Предаём серверу маршрутизатор запросов.
+	go func() {
+		log.Fatal(http.ListenAndServe("localhost:8081", srv.api.Router()))
+	}()
 	log.Println("HTTP server is started on localhost:8081")
-	defer log.Println("HTTP server has been stopped")
-	log.Fatal(http.ListenAndServe(":8081", srv.api.Router()))
+	signalCh := make(chan os.Signal, 1)
+	signal.Notify(signalCh, os.Interrupt)
+	<-signalCh
+	log.Println("HTTP server has been stopped")
 }
 
 //обрабатывает ответы из каналов с постами
